655d1533f38bb103e86b4344bab3fbba6841224c
Lethanial Leveille <leveillelethanial@gmail.com>
2026-04-25 18:35:44 -0400

    Initial Commit

---
+++ Nova/NovaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NovaApp.swift
+//  Nova
+//
+//  Created by Lethanial Leveille on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct NovaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
